cb702153182dafff69ddeb7b7b78bb48737f8762
nikki2107 <71547789+nikki2107@users.noreply.github.com>
2024-05-12 14:32:08 +0530

    Add files via upload

---
+++ js/Rotation.js
@@ -0,0 +1,54 @@
+AFRAME.registerComponent("diver-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 },
+    speedOfMovement: { type: "number", default: 0 }
+  },
+
+  init: function() {
+    window.addEventListener("keydown", (e) => {
+      this.data.speedOfRotation = this.el.getAttribute("rotation");
+      this.data.speedOfMovement = this.el.getAttribute("position");
+
+      var diverRotation = this.data.speedOfRotation;
+      var diverPosition = this.data.speedOfMovement;
+
+      if (e.key === "ArrowDown") {
+        if (diverPosition.z < 20) {
+          diverPosition.z += 0.1;
+          this.el.setAttribute("position", diverPosition)
+        }
+      }
+
+      if (e.key === "ArrowUp") {
+        if (diverPosition.z > -10) {
+          diverPosition.z -= 0.1;
+          this.el.setAttribute("position", diverPosition)
+        }
+      }
+
+      if (e.key === "ArrowRight") {
+        if (diverRotation.y > -360) {
+          diverRotation.y -= 0.5;
+          this.el.setAttribute("rotation", diverRotation)
+        }
+
+        if (diverPosition.x < 20) {
+          diverPosition.x += 0.05;
+          this.el.setAttribute("position", diverPosition)
+        }
+      }
+
+      if (e.key === "ArrowLeft") {
+        if (diverRotation.y < 360) {
+          diverRotation.y += 0.5;
+          this.el.setAttribute("rotation", diverRotation)
+        }
+
+        if (diverPosition.x > -20) {
+          diverPosition.x -= 0.05;
+          this.el.setAttribute("position", diverPosition)
+        }
+      }
+    });
+  }
+});
